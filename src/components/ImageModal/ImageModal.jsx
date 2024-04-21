acccbeaacdebd103edc3a0393f370b5c2281d174
bondalina <142501520+bondalina@users.noreply.github.com>
2024-04-21 22:19:31 +0300

    update

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -3,12 +3,6 @@ import css from "./ImageModal.module.css";
 
 const customStyles = {
   content: {
-    // top: "50%",
-    // left: "50%",
-    // right: "auto",
-    // bottom: "auto",
-    // marginRight: "-50%",
-    // transform: "translate(-50%, -50%)",
     padding: "0",
     border: "none",
     outline: "none",
