8fdcfe9326a42dfc08c660eb74438ef0ca2a66b4
bondalina <142501520+bondalina@users.noreply.github.com>
2024-04-23 14:09:32 +0300

    check

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,18 @@
 import css from "./ImageCard.module.css";
 
-const ImageCard = ({ urls, description }) => {
+const ImageCard = ({ urls, description, onClick }) => {
+  const handleClick = () => {
+    onClick(urls.regular);
+  };
+
   return (
     <div className={css.imageThumb}>
-      <img className={css.galleryImg} src={urls.small} alt={description} />
+      <img
+        className={css.galleryImg}
+        src={urls.small}
+        alt={description}
+        onClick={handleClick}
+      />
     </div>
   );
 };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,12 +5,12 @@ const ImageGallery = ({ items, onImageClick }) => {
   return (
     <ul className={css.galleryList}>
       {items.map((item) => (
-        <li
-          className={css.galleryItem}
-          key={item.id}
-          onClick={() => onImageClick(item.urls.regular)}
-        >
-          <ImageCard urls={item.urls} description={item.description} />
+        <li className={css.galleryItem} key={item.id}>
+          <ImageCard
+            urls={item.urls}
+            description={item.description}
+            onClick={() => onImageClick(item.urls.regular)}
+          />
         </li>
       ))}
     </ul>
