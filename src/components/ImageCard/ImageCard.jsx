2110b02f1b79d9bf79e7dbe21712429d8b6ed6dc
bondalina <142501520+bondalina@users.noreply.github.com>
2024-04-23 11:07:06 +0300

    update css

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,7 +1,11 @@
 import css from "./ImageCard.module.css";
 
 const ImageCard = ({ urls, description }) => {
-  return <img className={css.galleryImg} src={urls.small} alt={description} />;
+  return (
+    <div className={css.imageThumb}>
+      <img className={css.galleryImg} src={urls.small} alt={description} />
+    </div>
+  );
 };
 
 export default ImageCard;
